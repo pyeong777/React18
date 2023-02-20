74182e977f41afa31e7050e2e269b8147d8ddf0b
pyeong777 <sdp1123@naver.com>
2023-02-20 22:31:27 +0900

    Add: module css

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,8 @@
 import { FaTrashAlt } from "react-icons/fa";
+import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
-  const { text, status } = todo;
+  const { id, text, status } = todo;
 
   const handleChange = (e) => {
     const status = e.target.checked ? "completed" : "active";
@@ -13,17 +14,22 @@ export default function Todo({ todo, onUpdate, onDelete }) {
   };
 
   return (
-    <li>
+    <li className={styles.todo}>
       <input
+        className={styles.checkbox}
         type="checkbox"
-        id="checkbox"
+        id={id}
         checked={status === "completed"}
         onChange={handleChange}
       />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+      <label className={styles.text} htmlFor={id}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
