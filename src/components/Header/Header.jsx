9734fcfddcf0f64e2b46bccc18a3176d1ebb1bb7
pyeong777 <sdp1123@naver.com>
2023-02-20 22:30:01 +0900

    Add: toggle animation and darkmode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
 import styles from "./Header.module.css";
+import { HiMoon, HiSun } from "react-icons/hi";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((item, index) => (
           <li key={index}>
